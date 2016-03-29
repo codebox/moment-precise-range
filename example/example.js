54d38a2a0b342369d9d3e6d2677ad13c9b9dc52b
rob <rob@codebox.org.uk>
2016-03-29 09:37:26 +0100

    fixing npm package name

--- example/example.js
+++ example/example.js
@@ -1,5 +1,5 @@
 var moment = require('moment');
-require('moment-precise-range');
+require('moment-precise-range-plugin');
 
 var m1 = moment('2014-01-01 12:00:00','YYYY-MM-DD HH:mm:ss');
 var m2 = moment('2014-02-03 15:04:05','YYYY-MM-DD HH:mm:ss');
